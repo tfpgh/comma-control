6f7e9872ab08532f4a8eda55c90c7cbada53c89d
Toby Penner <tobypenner01@gmail.com>
2025-12-11 08:10:56 -0700

    Final finetuning params

--- controllers/net_cmaes_residual_4gpu.py
+++ controllers/net_cmaes_residual_4gpu.py
@@ -10,8 +10,8 @@ import torch.multiprocessing as mp
 
 # --- Configuration ---
 NUM_GPUS = 4
-POPULATION_SIZE = 512
-NUM_SEGMENTS = 250
+POPULATION_SIZE = 64
+NUM_SEGMENTS = 5000
 MAX_GENERATIONS = 10000
 INPUT_SIZE = 15
 HIDDEN_SIZE = 18
@@ -349,7 +349,7 @@ def main():
     x0 = np.load("residual_best_params.npy")
 
     es = cma.CMAEvolutionStrategy(
-        x0, 0.2, {"popsize": POPULATION_SIZE, "maxiter": MAX_GENERATIONS}
+        x0, 0.1, {"popsize": POPULATION_SIZE, "maxiter": MAX_GENERATIONS}
     )
 
     print("Optimization started...")
