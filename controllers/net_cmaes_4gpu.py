04de1a47f35c022cf8591b089b56b22bff2c3e2e
Toby Penner <tobypenner01@gmail.com>
2025-12-11 12:45:45 -0700

    Run fine tuning run

--- controllers/net_cmaes_4gpu.py
+++ controllers/net_cmaes_4gpu.py
@@ -10,7 +10,7 @@ from pathlib import Path
 # --- Configuration ---
 NUM_GPUS = 4
 POPULATION_SIZE = 512
-NUM_SEGMENTS = 64
+NUM_SEGMENTS = 250
 MAX_GENERATIONS = 10000
 INPUT_SIZE = 15
 HIDDEN_SIZE = 18
@@ -334,7 +334,7 @@ def main():
     x0 = np.load("4gpu_best_params.npy")
 
     es = cma.CMAEvolutionStrategy(
-        x0, 0.5, {"popsize": POPULATION_SIZE, "maxiter": MAX_GENERATIONS}
+        x0, 0.2, {"popsize": POPULATION_SIZE, "maxiter": MAX_GENERATIONS}
     )
 
     print("Optimization started...")
